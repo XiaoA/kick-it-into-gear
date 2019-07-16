ba9f1558956ed395ef9dc9530f9dda2db781131a
Andrew Buckingham <abuckingham99@gmail.com>
2019-07-16 13:59:19 -0700

    Display tasks on 'app/views/task/index.html.erb' page

--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -8,4 +8,9 @@ describe "index" do
   it "can be reached successfully" do
     expect(page.status_code).to eq(200)
   end
+
+  it "shows a list of tasks" do
+    expect(page).to have_content(/Task/)
+  end
+    
 end
