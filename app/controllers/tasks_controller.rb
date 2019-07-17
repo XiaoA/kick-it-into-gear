605665e3041f7e427463fb79b0b99de9b25184ed
Andrew Buckingham <abuckingham99@gmail.com>
2019-07-16 17:38:04 -0700

    Implement Tasks#Edit action and views

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -20,6 +20,19 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to @task, notice: 'Your task was updated.'
+    else
+      render :edit 
+    end
+  end
+  
   private
 
   def task_params
--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -1,26 +1,28 @@
 require "rails_helper"
 
-describe "index" do
-  before do
-    visit tasks_path
+describe "Task Actions", type: :feature do
+  describe "index" do
+    before do
+      visit tasks_path
+    end
+
+    it "can be reached successfully" do
+      expect(page.status_code).to eq(200)
+    end
+
+    it "shows a title of 'Tasks'" do
+      expect(page).to have_content(/Task/)
+    end
+
+    it "displays a list of tasks" do
+      first_task = Task.create(title: "Task 1", description: "Meow dui in ligula mollis ultricies.", status: 0)
+      second_task = Task.create(title: "Task 2", description: "Phasellus at dui in ligula mollis meow.", status: 0)
+
+      expect(Task.count).to eq(2)
+    end
   end
 
-  it "can be reached successfully" do
-    expect(page.status_code).to eq(200)
-  end
-
-  it "shows a title of 'Tasks'" do
-    expect(page).to have_content(/Task/)
-  end
-
-  it "has a list of tasks" do
-    first_task = Task.create(title: "Task 1", description: "Meow dui in ligula mollis ultricies.", status: 0)
-    second_task = Task.create(title: "Task 2", description: "Phasellus at dui in ligula mollis meow.", status: 0)
-
-    expect(Task.count).to eq(2)
-  end
-
-  describe "creation" do
+  describe "create" do
     before do
       visit new_task_path
     end
@@ -30,11 +32,11 @@ describe "index" do
     end
 
     it "can create a new task" do
-   
-    fill_in "Title", with: "Buy veggies"
-    fill_in "Description", with: "carrots, cabbage, and cucumbers"
-   
-    expect { click_on "Create Task" }.to change(Task, :count).by(1)
+      
+      fill_in "Title", with: "Buy veggies"
+      fill_in "Description", with: "carrots, cabbage, and cucumbers"
+      
+      expect { click_on "Create Task" }.to change(Task, :count).by(1)
     end
 
     it "redirects to the Index page after saving a new task" do
@@ -59,4 +61,18 @@ describe "index" do
       expect(page).to have_content(/buy a cat/)
     end
   end
+
+  describe "edit" do
+    before do
+      @task = Task.create(title: "Buy bread", description: "Get some bread.", status: 0)
+    end
+
+    it "can be edited" do
+      visit edit_task_path(@task)
+      find("#task_title").set("").set("Buy wheat bread")
+      click_on "Update Task"
+    end
+  end
 end
+
+
