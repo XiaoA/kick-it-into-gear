013cd336389b6e10ff792fd588c69fc9898e04ec
Andrew Buckingham <abuckingham99@gmail.com>
2019-07-21 20:35:52 -0700

    Reorder enums and sort by status on index page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,8 +2,8 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = current_user.tasks.all
-  end
+    @tasks = current_user.tasks.all.order("status ASC")
+   end
 
   def new
     @task = Task.new
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
 
-  enum status: { todo: 0, done: 1, waiting: 2, canceled: 3 }
+  enum status: { todo: 0, waiting: 1, done: 2, canceled: 3 }
   validates_presence_of :title
-
-                        
 end
