8dbadde7a5c1ecc8acb31cb000692d7692bab421
Andrew Buckingham <abuckingham99@gmail.com>
2019-07-22 06:58:25 -0700

    Change task status 'canceled' to 'someday'

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
 
-  enum status: { todo: 0, waiting: 1, done: 2, canceled: 3h }
+  enum status: { todo: 0, waiting: 1, done: 2, someday: 3 }
   validates_presence_of :title
 end
